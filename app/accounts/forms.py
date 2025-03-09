85263ba0dc6acba91513b6c42e2ba9afb0739cbd
Jonathan Biemond <jonbiemond@gmail.com>
2025-03-09 09:57:02 +0100

    format code

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -4,7 +4,6 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(AdminUserCreationForm):
-
     class Meta:
         model = CustomUser
         fields = (
@@ -14,7 +13,6 @@ class CustomUserCreationForm(AdminUserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
         fields = (
