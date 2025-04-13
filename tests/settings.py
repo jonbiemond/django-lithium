9c9f52dc9e45b5bda32980e756a8fef7c93777d7
Jonathan Biemond <jonbiemond@gmail.com>
2025-04-13 11:31:36 +0200

    add pytest-django dependency

---
+++ tests/settings.py
@@ -0,0 +1,12 @@
+from app.settings import *
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
